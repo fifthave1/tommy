ac91e08b17662ecc56d9983e71c60f392a83bb04
Kwon Yongkwang <fifthave1@gmail.com>
2021-01-11 18:57:05 +0900

    add .

--- python/test01_image.py
+++ python/test01_image.py
@@ -10,10 +10,10 @@ img = cv2.arrowedLine(img, (0, 255), (255, 0), (0, 255, 0), 3)  # bgr
 img = cv2.rectangle(img, (255, 255), (511, 511), (0, 0, 255), 2)
 img = cv2.circle(img, (255, 255), 100, (0, 255, 255), 2)
 font = cv2.FONT_HERSHEY_SIMPLEX
-img = cv2.putText(img, 'OpenCV', (10, 400), font, 4,
-                  (255, 255, 255), 19, cv2.LINE_AA)
+img = cv2.putText(img, 'OpenCV', (10, 400), font, 4,(255, 255, 255), 19, cv2.LINE_AA)
 
 cv2.imshow('tommy', img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+1
